59b0489da34e5d98e7fb1e38cfd8846d2b390402
Alva Chien <alvachien@live.com>
2017-02-09 23:25:54 +0800

    Initial commit

---
+++ src/main/java/com/alvachien/learning/java/acolingo2/App.java
@@ -0,0 +1,13 @@
+package com.alvachien.learning.java.acolingo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
